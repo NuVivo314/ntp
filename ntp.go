567f3b89639e18263f5bf8b4323ad891efa9ca9d
Dmitry Medvedev <eth@corbina.net>
2014-10-27 00:13:40 +0300

    rename Time to Request, return NtpStats as in py-ntplib, calculate net rtt delay and offset

--- ntp.go
+++ ntp.go
@@ -3,10 +3,13 @@
 // behavior and version 4 of the NTP protocol.  See RFC 5905.
 // Approach inspired by go-nuts post by Michael Hofmann:
 // https://groups.google.com/forum/?fromgroups#!topic/golang-nuts/FlcdMU5fkLQ
+
 package ntp
 
 import (
 	"encoding/binary"
+	"errors"
+	"math"
 	"net"
 	"time"
 )
@@ -29,9 +32,26 @@ type ntpTime struct {
 	Fraction uint32
 }
 
+type NtpStats struct {
+	Delay  time.Duration
+	Offset time.Duration
+}
+
 func (t ntpTime) UTC() time.Time {
 	nsec := uint64(t.Seconds)*1e9 + (uint64(t.Fraction) * 1e9 >> 32)
 	return time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC).Add(time.Duration(nsec))
+}
+
+func toNtpTime(t time.Time) ntpTime {
+	epoch := time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)
+	d := t.Sub(epoch)
+	i, f := math.Modf(d.Seconds())
+
+	n := new(ntpTime)
+	n.Seconds = uint32(i)
+	n.Fraction = uint32(f * math.Pow(2, 32)) // fraction * uint32 max value
+
+	return *n
 }
 
 type msg struct {
@@ -58,17 +78,28 @@ func (m *msg) SetMode(md mode) {
 	m.LiVnMode = (m.LiVnMode & 0xf8) | byte(md)
 }
 
-// Time returns the "receive time" from the remote NTP server
+func (m *msg) SetOriginTime(t ntpTime) {
+	m.OriginTime = t
+}
+
+func (m *msg) SetTransmitTime(t ntpTime) {
+	m.TransmitTime = t
+}
+
+// Request returns NTP stats: rtt delay and offset
+// from the remote NTP server
 // specifed as host.  NTP client mode is used.
-func Time(host string) (time.Time, error) {
+func Request(host string) (NtpStats, error) {
+	saneEpoch := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	stats := NtpStats{}
 	raddr, err := net.ResolveUDPAddr("udp", host+":123")
 	if err != nil {
-		return time.Now(), err
+		return stats, err
 	}
 
 	con, err := net.DialUDP("udp", nil, raddr)
 	if err != nil {
-		return time.Now(), err
+		return stats, err
 	}
 	defer con.Close()
 	con.SetDeadline(time.Now().Add(5 * time.Second))
@@ -76,17 +107,40 @@ func Time(host string) (time.Time, error) {
 	m := new(msg)
 	m.SetMode(client)
 	m.SetVersion(4)
+	originTime := time.Now() // time client sent request
+	m.SetTransmitTime(toNtpTime(originTime))
 
 	err = binary.Write(con, binary.BigEndian, m)
 	if err != nil {
-		return time.Now(), err
+		return stats, err
 	}
 
 	err = binary.Read(con, binary.BigEndian, m)
 	if err != nil {
-		return time.Now(), err
+		return stats, err
 	}
 
-	t := m.ReceiveTime.UTC().Local()
-	return t, nil
+	destinationTime := time.Now() // time client got reply
+
+	receiveTime := m.ReceiveTime.UTC()   // time server got request
+	transmitTime := m.TransmitTime.UTC() // time server scheduled reply
+
+	if receiveTime.Before(saneEpoch) || transmitTime.Before(saneEpoch) {
+		return stats, errors.New("received zero packet")
+	}
+
+	// check that server replies to our request
+	if m.OriginTime != toNtpTime(originTime) {
+		return stats, errors.New("received bogus packet")
+	}
+
+	netRttDelay := destinationTime.Sub(originTime)
+	srvSchedDelay := transmitTime.Sub(receiveTime)
+	delay := netRttDelay - srvSchedDelay
+
+	offset := (receiveTime.Sub(originTime) + transmitTime.Sub(destinationTime)) / 2
+
+	stats = NtpStats{delay, offset}
+	return stats, nil
+
 }
